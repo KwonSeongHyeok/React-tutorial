fd8a5aac222a4a5d83b0d001b4d37f77c574e07a
KwonSeongHyeok <aiqksiwk@naver.com>
2024-07-11 12:49:24 +0900

    useRefInput

--- tutorial-one/src/component/R20_useRefInput.js
+++ tutorial-one/src/component/R20_useRefInput.js
@@ -3,9 +3,22 @@ import React, {useRef, useEffect} from 'react';
 const RefInputFocus = () => {
     const inputRef = useRef(null);
 
-    const 버튼클릭 = () => {
-        if(inputRef.current){
+    // useEffect에서, [] 값이 비어있으면 R20_useRefInput.js를 시작하자마자
+    // 특정 기능이나 효과 발생
+    // R20_useRefInput.js를 들어오자마자 input창을 바라보기 설정
+    useEffect (() => {
+        // current = null이기 때문에 inputRef = null 참
+        if(inputRef.current){ // current = 기본값 null
             inputRef.current.focus();
+        }
+    }, []);
+
+    const 버튼클릭 = () => {
+        if(inputRef.current){ // current 는 inputRef가 설정된 태그값 확인
+            inputRef.current.focus(); // focus는 영어로 초점
+            // inputRef라는 ref값을 가진
+            // current = input 태그를 바라보고
+            // focus() = input이 null 이라면 input창 안을 중점으로 확인
         }
     };
 
