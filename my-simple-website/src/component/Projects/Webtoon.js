52cdd861fc1a8451a1d8b192e7d654a176dd5627
KwonSeongHyeok <aiqksiwk@naver.com>
2024-07-22 10:15:33 +0900

    yarn practice

---
+++ my-simple-website/src/component/Projects/Webtoon.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Webtoon = () => {
+    
+    return(
+        <div className='webtoon-container'>
+            <div className='webtoon-about'>
+                <h2>K-toon(전지적 개발자 시점)</h2>
+                <p></p>
+            </div>
+        
+            <p>웹툰 소개 웹툰 이미지 웹툰 gif 웹툰 작성</p>
+        </div>
+    )
+}
+export default Webtoon;
